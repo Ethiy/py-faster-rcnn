df5667673bd08415665f6d8ae3bcb4665d78eb69
Ethiy <oussama.ennafii@polytechnique.org>
2016-07-05 15:47:21 +0000

    adapting for letters database

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -11,6 +11,7 @@ __sets = {}
 
 from datasets.pascal_voc import pascal_voc
 from datasets.coco import coco
+from datasets.letters import letters
 import numpy as np
 
 # Set up voc_<year>_<split> using selective search "fast" mode
@@ -30,6 +31,12 @@ for year in ['2015']:
     for split in ['test', 'test-dev']:
         name = 'coco_{}_{}'.format(year, split)
         __sets[name] = (lambda split=split, year=year: coco(split, year))
+
+letters_devkit_path = '/home/ubuntu/3rdParty/ConvNets/py-faster-rcnn/data/Letters_devkit'
+for split in ['train', 'test']:
+    name = '{}_{}'.format('letters', split)
+    __sets[name] = (lambda split=split: letters(split, letters_devkit_path))
+
 
 def get_imdb(name):
     """Get an imdb (image database) by name."""
